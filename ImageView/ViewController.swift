d2663f42eb9f19a4eb02cca61ca52b02169b1630
gunrpark <gunrpark@snu.ac.kr>
2022-09-04 00:19:31 +0900

    test

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImagOnOff(_ sender: UISwitch) {
+    }
 }
 
